f26d61b7b3d7c27adf06b1793a708b4e8bb532d5
liv <jonas.streun@outlook.com>
2024-04-26 21:06:23 +0200

    introduce creepRoles

---
+++ src/roadScheduler.js
@@ -0,0 +1,34 @@
+﻿const roadScheduler = {
+    run() {
+        // get one creep in each room
+        let oneCreepPerRoom;
+        
+        for (let creep in Game.creeps) {
+            if (oneCreepPerRoom[Game.creeps[creep].room.name])
+            {
+                
+            }
+        }
+        
+
+        // loop over one creep per room
+        for (let creep in oneCreepPerRoom) {
+            // get spawner location
+            const spawner = Game.spawns.filter(spawn => spawn.room.name === creep.room.name);
+            const controller = spawner.room.controller;
+            // get sources in room
+            const sources = creep.pos.find(FIND_SOURCES);
+
+            for(let source in sources) {
+                const path = creep.findPath(spawner.pos, source.pos).append(creep.findPath(controller.pos, source.pos));
+                const roadless = path.filter((step) => !RoomPosition(step.x, step.y, creep.room.name).look()[1].structure);
+
+                //RoomPosition(roadless.x, roadless.y, creep.room.name);
+                roadless.map(toBuild => toBuild.pos.createConstructionSite(STRUCTURE_ROAD));
+            }
+        }    
+    }
+    
+}
+
+module.exports = roadScheduler;
